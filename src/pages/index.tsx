f96f1236b5aeae0146f4d52f8e80ab643231dc61
Soskxkakr <aldo.taslim@gmail.com>
2023-12-17 16:12:38 +0800

    Update icon and meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,10 +15,22 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Reinaldo Taslim</title>
-        <meta name="description" content="Reinaldo Taslim | Software Engineer" />
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+
+        <title>Reinaldo Taslim | Software Engineer</title>
+        
+        <meta name="description" content="Reinaldo Taslim | Software Engineer" />
+        <meta name="keywords" content="Soskxkakr, Reinaldo, Reinaldo Taslim, Software Engineer, Software Developer" />
+
+        <meta key="og:type" property="og:type" content="website" />
+        <meta key="og:title" property="og:title" content="Reinaldo Taslim" />
+        <meta key="og:description" property="og:description" content="Reinaldo Taslim | Software Enginner" />
+        <meta key="og:url" property="og:url" content="https://soskxkakr.vercel.app" />
+        <meta key="og:image" property="og:image" content="https://soskxkakr.vercel.app/_next/image?url=%2Fme.jpg&w=384&q=75" />
       </Head>
       <ThemeProvider>
         <main className={`${inter.className}`}>
